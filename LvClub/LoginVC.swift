0b726fd306c91f8cf3c22993b3f64f5ab51b8899
Med Beji <medbeji@MBPMED.local>
2016-12-04 13:00:35 +0100

    Created Login View

---
+++ LvClub/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  LvClub
+//
+//  Created by Med Beji on 04/12/2016.
+//  Copyright © 2016 Med Beji. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
